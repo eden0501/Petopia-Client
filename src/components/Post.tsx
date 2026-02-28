43c0876c0a9ccd8801dc04b80d2b2f46ab9e0d93
eden0501 <eden03.b@gmail.com>
2026-02-28 16:40:38 +0200

    add comments box

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import PostComments from "./PostComments";
 import PostTypeChip from "./PostTypeChip";
 import { formatDistanceToNowStrict } from "date-fns";
 import {
@@ -29,67 +31,77 @@ const Post = ({
   title,
   type,
   hashtags,
-}: PostInterface) => (
-  <Card>
-    <CardHeader
-      avatar={<Avatar src={author.profilePicture} />}
-      title={author.username}
-      subheader={formatDistanceToNowStrict(createdAt, { addSuffix: true })}
-      sx={{
-        ".MuiCardHeader-title": {
-          fontSize: "1rem",
-          fontWeight: "600",
-        },
-      }}
-    />
-    <CardContent sx={{ paddingTop: 0 }}>
-      <PostTypeChip postType={type} />
-      <Typography
-        sx={{
-          marginTop: 5,
-          fontWeight: "600",
-          marginBottom: 20,
-        }}
-      >
-        {title}
-      </Typography>
-      <Stack sx={{ gap: 10 }}>
-        <Typography sx={{ color: "text.secondary" }}>{content}</Typography>
-        {imageUrl && (
-          <CardMedia sx={{ borderRadius: 2 }} component="img" src={imageUrl} />
-        )}
-        {!!hashtags?.length && (
-          <Typography sx={{ color: "primary.main", fontSize: "0.9rem" }}>
-            #{hashtags.join(" #")}
-          </Typography>
-        )}
-      </Stack>
-    </CardContent>
-    <Divider />
-    <CardActions sx={{ padding: 20, justifyContent: "space-around" }}>
-      <Button
-        variant="text"
-        startIcon={liked ? <FavoriteOutlined /> : <Favorite />}
-        sx={{
-          color: liked ? "red" : "text.secondary",
-          textTransform: "none",
-        }}
-      >
-        Likes
-      </Button>
+}: PostInterface) => {
+  const [openComments, setOpenComments] = useState(false);
 
-      <Button
-        variant="text"
-        startIcon={<Comment />}
-        sx={{
-          color: "text.secondary",
-          textTransform: "none",
-        }}
-      >
-        Comments
-      </Button>
-    </CardActions>
-  </Card>
-);
+  return (
+    <>
+      <Card>
+        <CardHeader
+          avatar={<Avatar src={author.profilePicture} />}
+          title={author.username}
+          subheader={formatDistanceToNowStrict(createdAt, { addSuffix: true })}
+          sx={{
+            ".MuiCardHeader-title": {
+              fontSize: "1rem",
+              fontWeight: "600",
+            },
+          }}
+        />
+        <CardContent sx={{ paddingTop: 0 }}>
+          <PostTypeChip postType={type} />
+          <Typography
+            sx={{
+              marginTop: 5,
+              fontWeight: "600",
+              marginBottom: 20,
+            }}
+          >
+            {title}
+          </Typography>
+          <Stack sx={{ gap: 10 }}>
+            <Typography sx={{ color: "text.secondary" }}>{content}</Typography>
+            {imageUrl && (
+              <CardMedia
+                sx={{ borderRadius: 2 }}
+                component="img"
+                src={imageUrl}
+              />
+            )}
+            {!!hashtags?.length && (
+              <Typography sx={{ color: "primary.main", fontSize: "0.9rem" }}>
+                #{hashtags.join(" #")}
+              </Typography>
+            )}
+          </Stack>
+        </CardContent>
+        <Divider />
+        <CardActions sx={{ padding: 20, justifyContent: "space-around" }}>
+          <Button
+            variant="text"
+            startIcon={liked ? <FavoriteOutlined /> : <Favorite />}
+            sx={{
+              color: liked ? "red" : "text.secondary",
+            }}
+          >
+            Likes
+          </Button>
+
+          <Button
+            variant="text"
+            startIcon={<Comment />}
+            onClick={() => setOpenComments((prev) => !prev)}
+            sx={{
+              color: openComments ? "primary.main" : "text.secondary",
+            }}
+          >
+            Comments
+          </Button>
+        </CardActions>
+      </Card>
+      {openComments && <PostComments />}
+    </>
+  );
+};
 
 export default Post;
